af025874a2fb3cba10ba2bd3a9805b75b922bd61
Azamatjon17 <153727489+Azamatjon17@users.noreply.github.com>
2024-07-02 19:17:33 +0500

    df

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/controllers/card_controller.dart';
+import 'package:flutter_application_1/firebase_options.dart';
 import 'package:flutter_application_1/service/product_firebases.dart';
 import 'package:flutter_application_1/views/screens/firspage.dart';
+import 'package:flutter_application_1/views/screens/login_page.dart';
 import 'package:provider/provider.dart';
 
-void main(List<String> args) {
+void main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(MyApp());
 }
 
@@ -26,7 +35,16 @@ class _MyAppState extends State<MyApp> {
         ],
         builder: (context, child) {
           return MaterialApp(
-            home: Firspage(),
+            home: StreamBuilder(
+              stream: FirebaseAuth.instance.userChanges(),
+              builder: (context, user) {
+                if (user.data == null) {
+                  return LoginPage();
+                } else {
+                  return Firspage();
+                }
+              },
+            ),
           );
         });
   }
